b907d54a38df49afa1d26b4d3d6ed824eb07507c
Don Sirivat <dsirivat17@gmail.com>
2017-10-11 13:45:19 -0400

    redesigned flow

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ let restIds = [];
 let latitude = 0;
 let longitude = 0;
 
+$('#startBtn').on('click', event => {
+  $('.loader').show();
+  $('#start-section').hide();
+  $('#result-section').show();
+  getDataFromApi("", latitude, longitude, displayData);
+})
+
+$('#nahBtn').on('click', event => {
+  $('.loader').show();
+  let randomNumber = Math.floor((Math.random() * 20) + 0);
+  getRestaurantInfo(restIds[randomNumber], displayRestaurant);
+})
+
 function displayRestaurant(data) {
   console.log(data);
-  $('#restImage').attr('src', data.featured_image);
+  $('.loader').hide();
   $('#restName').text(data.name);
   $('#address').text(data.location.address);
-  $('#averageCost').text("Average Cost For Two: $" + data.average_cost_for_two);
-  $('#cuisine').text(data.cuisines);
-  $('#rating').text("Average Rating: " + data.user_rating.aggregate_rating + " (" + data.user_rating.rating_text + ")");
-  $('.loader').hide();
-  $('#start-section').hide();
-  $('.result-section').show();
+  $('#averageNum').text(data.average_cost_for_two);
+  $('#restCat').text(data.cuisines);
+  $('#restImage').attr('src', data.featured_image);
 }
 
-function getRestaurantInfo(query, callback) {
+function  getRestaurantInfo(query, callback) {
   const settings = {
     headers: {
       'user-key': '71d3b2da9be5426b8548cb6255342503'
@@ -31,64 +41,58 @@ function getRestaurantInfo(query, callback) {
   $.ajax(settings);
 }
 
-$('#nahBtn').on('click', event => {
-  $('.loader').show();
-  let randomNumber =Math.floor((Math.random() * 20) + 0);
-  getRestaurantInfo(restIds[randomNumber], displayRestaurant);
-})
-
 function displayData(data) {
   console.log(data.restaurants.length);
   for(let i=0;i<data.restaurants.length;i++) {
     restIds.push(data.restaurants[i].restaurant.id);
   }
   console.log(restIds);
-  let randomNumber =Math.floor((Math.random() * 20) + 0);
+  let randomNumber = Math.floor((Math.random() * 20) + 0);
   getRestaurantInfo(restIds[randomNumber], displayRestaurant);
 }
-
-function getDataFromApi(query, callback) {
-  const settings = {
-    headers: {
-      'user-key': '71d3b2da9be5426b8548cb6255342503'
-    }, 
-    data: {
-      q: query,
-      lat: latitude,
-      lon: longitude
-    },
-    dataType: 'json',
-    url: `https://developers.zomato.com/api/v2.1/search`,
-    type: 'GET',
-    success: callback
-  };
-  
-  $.ajax(settings);
-}
-
-$('#startBtn').on('click', event => {
-  console.log('Hi');
-  $('.loader').show();
-  let userQuery = $('#user-input').val();
-  getDataFromApi(userQuery, displayData);
-})
 
 function getLocation() {
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
   } else {
     console.log("Something went wrong");
+    renderWSIE();
   }
+}
+
+function getDataFromApi(query, lat, long, callback) {
+  const settings = {
+    headers: {
+    'user-key': '71d3b2da9be5426b8548cb6255342503'
+  },
+  data: {
+    q: query,
+    lat: lat,
+    lon: long
+  },
+  dataType: 'json',
+  url: `https://developers.zomato.com/api/v2.1/search`,
+  type: 'GET',
+  success: callback
+};
+
+$.ajax(settings);
 }
 
 function showPosition(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
-}
-function renderWSIE() {
+
   $('.loader').hide();
-  $('.result-section').hide();
+  console.log(latitude);
+  console.log(longitude);
+}
+
+function renderWSIE() {
+  $('#result-section').hide();
+  $('.loader').hide();
   getLocation();
+  $('.loader').show();
 }
 
 $(renderWSIE);
